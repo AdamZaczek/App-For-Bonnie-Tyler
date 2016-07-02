e6b0b89808c8fa755729c1f15878b1c15cb7f0a8
AdamZaczek <adamzaczekul@gmail.com>
2016-07-03 00:50:51 +0200

    Create Reusable Hero Detail Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 export class Hero {
   id: number;
   name: string;
@@ -27,14 +29,7 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
     .selected {
@@ -85,6 +80,7 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `]
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
